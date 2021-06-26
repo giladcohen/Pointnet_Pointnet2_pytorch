edecea1647f86129d006ef301e5ba99c210c2d91
giladcohen <giladadlig@gmail.com>
2021-06-26 15:48:13 +0300

    fixing loss printing in test()

--- models/pointnet_cls.py
+++ models/pointnet_cls.py
@@ -10,10 +10,9 @@ class PointNet(nn.Module):
         self.fc1 = nn.Linear(1024, 512)
         self.fc2 = nn.Linear(512, 256)
         self.fc3 = nn.Linear(256, k)
-        self.dropout = nn.Dropout(p=0.4)
+        self.dropout = nn.Dropout(p=0.0)  # was 0.4
         self.bn1 = nn.BatchNorm1d(512)
         self.bn2 = nn.BatchNorm1d(256)
-        self.relu = nn.ReLU()
 
     def forward(self, x):
         x, trans_feat = self.feat(x)
